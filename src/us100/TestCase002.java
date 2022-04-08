eedbb506e428f425d0b736571220cd4d2added3e
hrnoksz <harun1973@yahoo.com>
2022-04-08 10:25:03 +0300

    work 2 is done

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -5,5 +5,7 @@ public class TestCase002 {
     public static void main(String[] args) {
 
         System.out.println("I have to learn git and git-hub");
+
+        System.out.println("But there are too difficult to learn ");
     }
 }

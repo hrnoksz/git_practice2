604580ad7e38646c1a2548c4d1a7e75e2d26ad70
hrnoksz <harun1973@yahoo.com>
2022-04-08 10:18:46 +0300

    task 2 is done

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -5,6 +5,8 @@ public class TestCase001 {
     public static void main(String[] args) {
 
         System.out.println("Today is beautiful");
+
+        System.out.println("I am hanging out with my friends");
     }
 
 }
